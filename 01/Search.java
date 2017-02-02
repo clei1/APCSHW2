05c6cf9a5546c289f683e81bcee24cfcbdea6248
clei1 <clei1@stuy.edu>
2017-02-02 09:34:05 -0500

    finished hw02)

--- 01/Search.java
+++ 01/Search.java
@@ -5,6 +5,7 @@
 // 2017-02-02
 
 public class Search{
+    /*
     public static String searchMatrix(int[][] matrix, int target){
 	int row = 0;
 	int col = 0;
@@ -31,13 +32,18 @@ public class Search{
 	// still boggles my mind
 	// i just need this here so i can find out how to make it work for this one...
     }
-
-    public static String searchMatrix1(int[][] matrix, int target){
-	int row = 0
+    */
+    
+    public static String searchMatrix(int[][] matrix, int target){
+	int iteration = 0;
+	int row = 0;
 	int col = matrix.length - 1;
 	while(col >= 0 &&
 	      row >= 0 &&
 	      row < matrix.length){
+	    System.out.println("(" + row + "," + col + ")");
+	    iteration ++;
+	    System.out.println(iteration);
 	    if(matrix[row][col] == target){
 		return ("(" + row + "," + col + ")");
 	    }
@@ -49,5 +55,16 @@ public class Search{
 	    }
 	}
 	return "Target number could not be found in the matrix.";
+    }
+
+    public static void main(String[] args){
+	int[][] bey = {
+	    { 1,  3,  5 },
+	    { 3,  7,  8 },
+	    { 6, 12, 15 },
+	};
+	System.out.println(searchMatrix(bey, 6));
+	System.out.println(searchMatrix(bey, 9));
+	//System.out.println(searchMatrix(bey, 15));
     }
 }
---
+++ 02/MatrixFinder.java
@@ -0,0 +1,50 @@
+//ViCtory
+//Connie Lei && Vernita Lawren
+//HW#02 -- I Am Still Searching
+//2016-02-02
+
+public class MatrixFinder{
+
+    /*
+      You start from the top right corner. You have three options: 
+      1) You can return the coordinates if the target matches
+      2) You can increase the number by moving down, increasing vertically
+      3) You can decrease the number by moving left, decreasing horizontally
+      It is a zigzag pattern moving on the diagonal.
+      The worst case scenario is O(2n-1) and best case is O(1).
+     */
+
+    public static String searchMatrix(int[][] matrix, int target){
+	int iteration = 0;
+	int row = 0;
+	int col = matrix.length - 1;
+	while(col >= 0 &&
+	      row >= 0 &&
+	      row < matrix.length){
+	    //System.out.println("(" + row + "," + col + ")");
+	    iteration ++;
+	    //System.out.println(iteration);
+	    if(matrix[row][col] == target){
+		return ("(" + row + "," + col + ")");
+	    }
+	    else if(matrix[row][col] > target){
+		col --;
+	    }
+	    else if(matrix[row][col] < target){
+		row ++;
+	    }
+	}
+	return "(-1, -1)";
+    }
+
+    public static void main(String[] args){
+	int[][] bey = {
+	    { 1,  3,  5 },
+	    { 3,  7,  8 },
+	    { 6, 12, 15 },
+	};
+	System.out.println(searchMatrix(bey, 6));
+	System.out.println(searchMatrix(bey, 9));
+	//System.out.println(searchMatrix(bey, 15));
+    }
+}
